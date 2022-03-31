7246e2fceabce77f79a189a1a7bd94df76aa489f
David Beauchesne <david.beauchesne@uqar.ca>
2022-03-31 12:28:59 -0400

    aoi stations fig

---
+++ R/fig_aoi.R
@@ -0,0 +1,16 @@
+library(sf)
+library(mapview)
+sections <- st_read("./data/data-raw/saguenay_sections.geojson")
+stations <- st_read("./data/data-output/stations.geojson")
+
+cols <- c("#4B0055","#00588B","#009B95","#53CC67","#FDE333")
+nS <- c(20,15,50,20,20)
+pts <- list()
+for(i in 1:5) pts[[i]] <- rep(cols[i], nS[i])
+pts <- unlist(pts)
+
+png('./figures/saguenay.png', res = 300, width = 500, height = 225, units = "mm")
+par(mar = c(0,0,0,0))
+plot(st_geometry(sections), col = paste0(cols,"44"))
+plot(stations, add = TRUE, pch = 20, border = pts, col = paste0(pts,"99"), cex = 2.5, lwd = 2)
+dev.off()
